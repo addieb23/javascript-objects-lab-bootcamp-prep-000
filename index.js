b2b94ace114d712dc3e7068b83850ebd638df48c
Addie Brunk <mlbrunk23@gmail.com>
2018-05-28 18:52:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var recipes = {
   lunch: 'pbandj sandwich'
 }
 
-function updateObjectWithKeyAndValue(recipes, key, value){
-  recipes[key] = value;
-  return recipes
+function updateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object
 }
 
 //function
